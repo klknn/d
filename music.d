02db09ecac594e79f1086686e5744722b71d7b42
klknn <klknn.gh@gmail.com>
2023-10-08 03:03:18 +0900

    gen wav

--- music.d
+++ music.d
@@ -1,34 +1,36 @@
+import std.logger;
 import std.stdio;
 import std.math;
 
 /// http://soundfile.sapp.org/doc/WaveFormat/
-align(1) struct RIFF {
+align(1)
+struct RIFF {
   char[4] chunkId = "RIFF";
-  int chunkSize;
+  uint chunkSize;
   char[4] format = "WAVE";
   char[4] subChunk1Id = "fmt ";
-  int subChunk1Size = 16;
-  short audioFormat = 1; // 1:PCM, 3:float
-  short numChannels;
-  int sampleRate;
-  int byteRate;
-  short blockAlign;
-  short bitsPerSample;
+  uint subChunk1Size = 16;
+  ushort audioFormat = 1; // 1:PCM, 3:float
+  ushort numChannels;
+  uint sampleRate;
+  uint byteRate;
+  ushort blockAlign;
+  ushort bitsPerSample;
   char[4] subChunk2Id = "data";
-  int subChunk2Size;
+  uint subChunk2Size;
 }
 
 void writeWav(T = short)(float[][] wav, int sampleRate, string outputPath) {
   auto numChannels = wav.length;
   auto numSamples = wav[0].length;
   RIFF header;
-  header.numChannels = cast(short) numChannels;
+  header.numChannels = cast(ushort) numChannels;
   header.sampleRate = sampleRate;
-  header.byteRate = cast(int)(sampleRate * numChannels * T.sizeof);
-  header.blockAlign = cast(short)(numChannels * T.sizeof);
-  header.bitsPerSample = cast(short) T.sizeof * 8;
-  header.subChunk2Size = cast(int)(numSamples * header.numChannels * T.sizeof);
-  header.chunkSize = 36 + header.subChunk2Size;
+  header.byteRate = cast(uint)(sampleRate * numChannels * T.sizeof);
+  header.blockAlign = cast(ushort)(numChannels * T.sizeof);
+  header.bitsPerSample = cast(ushort) T.sizeof * 8;
+  header.subChunk2Size = cast(uint)(numSamples * header.numChannels * T.sizeof);
+  header.chunkSize = header.subChunk2Size + cast(uint)(RIFF.sizeof - 8);
   FILE* fp = fopen(outputPath.ptr, "wb");
   scope (exit) {
     fclose(fp);
@@ -42,12 +44,26 @@ void writeWav(T = short)(float[][] wav, int sampleRate, string outputPath) {
   }
 }
 
-void main() {
-  int sampleRate = 44100;
-  float[][] wav = new float[][](sampleRate * 3, 2);
-  foreach (t; 0 .. wav[0].length) {
-    wav[0][t] = sin(2.0 * PI * 440 / sampleRate);
-    wav[1][t] = wav[0][t];
+void main(string[] args) {
+  int sampleRate = 44_100;
+  float[][] wav; //  = new float[][](2, sampleRate * 3);
+  wav.length = 2;
+  foreach (ref w; wav) {
+    w.length = sampleRate * 10;
   }
-  writeWav(wav, sampleRate, "output.wav");
+  info("#channels: ", wav.length);
+  info("#frames: ", wav[0].length);
+  foreach (t; 0 .. wav[0].length) {
+    wav[0][t] = 0;
+    wav[1][t] = 0;
+    if ((cast(float) t / sampleRate % 0.5) < 0.1)
+      continue;
+    foreach (i; 1 .. 8) {
+      wav[0][t] += 0.5 / i * sin(2.0 * PI * (44 + t * 0.01) * i * t / sampleRate);
+      wav[1][t] += 0.5 * sin(2.0 * PI * 66 * i * t / sampleRate);
+    }
+    // wav[1][t] = wav[0][t];
+    trace("time: ", t, " wav:", wav[0][t]);
+  }
+  writeWav(wav, sampleRate, args.length > 1 ? args[1] : "/dev/stdout");
 }
